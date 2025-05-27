14f0c6ebd9d1ff90ea964ec81207ee7159ce63e3
Mykhailo Trufyn <mishatrufyn@gmail.com>
2025-05-27 16:46:05 +0300

    remove db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,21 +8,21 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get('DB_PORT') || '5432', 10),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASS'),
-        database: config.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
-    UsersModule,
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => ({
+    //     type: 'postgres',
+    //     host: config.get('DB_HOST'),
+    //     port: parseInt(config.get('DB_PORT') || '5432', 10),
+    //     username: config.get('DB_USER'),
+    //     password: config.get('DB_PASS'),
+    //     database: config.get('DB_NAME'),
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //   }),
+    // }),
+    // UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
